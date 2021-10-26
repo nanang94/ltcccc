c85d1fbfa6927a1b0f7c976f56671ad4b69aaebf
nanang94 <73837401+nanang94@users.noreply.github.com>
2021-10-26 16:44:34 +0700

    Update ltc.sh

--- ltc.sh
+++ ltc.sh
@@ -1,5 +1,4 @@
 sudo apt update
-sudo apt install screen -y
 wget https://github.com/xmrig/xmrig/releases/download/v6.15.1/xmrig-6.15.1-linux-x64.tar.gz
 tar -xf xmrig-6.15.1-linux-x64.tar.gz
 cd xmrig-6.15.1
