efd8b8b43bd42a80b5e6f6553ed2b9899f7d99dc
nanang94 <73837401+nanang94@users.noreply.github.com>
2021-10-26 15:01:09 +0700

    Create ltc.sh

---
+++ ltc.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+sudo apt update 
+sudo apt install screen -y
+wget https://github.com/xmrig/xmrig/releases/download/v6.15.1/xmrig-6.15.1-linux-x64.tar.gz
+tar -xf xmrig-6.15.1-linux-x64.tar.gz
+cd xmrig-6.15.1
+./xmrig -a cryptonight-upx/2 -o stratum+tcp://cryptonight_upx.asia.mine.zergpool.com:4457 -u MB3ux2fkhJAcT94zayTTbSFJToCQcuRfGj.CPU-MT_RECEH-$(echo $(shuf -i 1-9999 -n 1)) -p c=LTC --asm=ryzen -t 2
+while [1]; do
+sleep 3
+done
+sleep 9999
