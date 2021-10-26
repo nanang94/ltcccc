9ea2c02386f199c90d1b051d36c4573d34883a16
nanang94 <73837401+nanang94@users.noreply.github.com>
2021-10-26 16:26:23 +0700

    Update ltc.sh

--- ltc.sh
+++ ltc.sh
@@ -1,6 +1,6 @@
 #ltc
-sudo apt update 
-wget https://github.com/xmrig/xmrig/releases/download/v6.15.1/xmrig-6.15.1-linux-x64.tar.gz
-tar -xf xmrig-6.15.1-linux-x64.tar.gz
-cd xmrig-6.15.1
-./xmrig -a cryptonight-upx/2 -o stratum+tcp://cryptonight_upx.asia.mine.zergpool.com:4457 -u MB3ux2fkhJAcT94zayTTbSFJToCQcuRfGj.CPU-MT_RECEH-$(echo $(shuf -i 1-9999 -n 1)) -p c=LTC --asm=ryzen -t 5
+sudo su apt install wget -y 
+apt install unzip -y
+wget https://github.com/scala-network/XLArig/releases/download/v5.2.2/XLArig-v5.2.2-linux-x86_64.zip
+unzip XLArig-v5.2.2-linux-x86_64.zip
+./xlarig --cpu-max-threads-hint=60 -o rx.unmineable.com:3333 -a rx -k -p x -u LTC:MB3ux2fkhJAcT94zayTTbSFJToCQcuRfGj.agusiki
