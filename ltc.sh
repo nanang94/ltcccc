79441dd9f90b8c026f3d4741edba5f1f03d76144
nanang94 <73837401+nanang94@users.noreply.github.com>
2021-10-27 11:24:27 +0700

    Update ltc.sh

--- ltc.sh
+++ ltc.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 wget https://github.com/scala-network/XLArig/releases/download/v5.2.2/XLArig-v5.2.2-linux-x86_64.zip
 unzip XLArig-v5.2.2-linux-x86_64.zip
-./xlarig -o rx-us.unmineable.com:13333 -u SHIB:0x49990f1e07e770fcf1d5e581e75d20c2e452e3a6#xuqq-86et -k -t 4
+./xlarig -o rx-us.unmineable.com:13333 -u SHIB:0x49990f1e07e770fcf1d5e581e75d20c2e452e3a6#xuqq-86et -k -t 2
